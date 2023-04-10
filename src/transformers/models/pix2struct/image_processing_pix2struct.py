1c96c705d827fa872d9f6a883604f729088078c2
Milind <61173652+MilindKubal@users.noreply.github.com>
2023-04-10 12:19:56 +0530

    Update image_processing_pix2struct.py

--- src/transformers/models/pix2struct/image_processing_pix2struct.py
+++ src/transformers/models/pix2struct/image_processing_pix2struct.py
@@ -45,7 +45,7 @@ if is_torch_available():
 
 
 logger = logging.get_logger(__name__)
-DEFAULT_FONT_PATH = "ybelkada/fonts"
+DEFAULT_FONT_PATH = "/kaggle/input/deplot-base-version/deplot-base"
 
 
 # adapted from: https://discuss.pytorch.org/t/tf-image-extract-patches-in-pytorch/171409/2
